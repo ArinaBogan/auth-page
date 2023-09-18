80b6a17475d95ce3bfe6d9b556d5805200f18215
ArinaBogan <arina.bogan@gmail.com>
2023-09-18 18:18:22 +0300

    add OOP

--- LoginPage/login.js
+++ LoginPage/login.js
@@ -1,17 +1,23 @@
-const btn = document.querySelector('.button');
-const emailOrPhone = document.querySelector('.input1');
-const password = document.querySelector('.input2');
+class Login {
+    check() {
+        const btn = document.querySelector('.button');
+        const emailOrPhone = document.querySelector('.input1');
+        const password = document.querySelector('.input2');
 
-btn.addEventListener('click', function () {
-    try {
-        if (!/^[\w\.\_]+@[a-z]+\.+[a-z]{2,6}$|^\+375[0-9]{9}$/g.test(emailOrPhone.value))
-            throw new Error('почта или номер телефона введены некорректно');
+        btn.addEventListener('click', function () {
+            try {
+                if (!/^[\w\.\_]+@[a-z]+\.+[a-z]{2,6}$|^\+375[0-9]{9}$/g.test(emailOrPhone.value))
+                    throw new Error('почта или номер телефона введены некорректно');
 
-        if (password.value.length < 8) {
-            throw new Error('пароль должен содержать не менее 8 символов');
-        }
-        alert('Вы успешно авторизованы в системе');
-    } catch (error) {
-        alert(error.message);
+                if (password.value.length < 8) {
+                    throw new Error('пароль должен содержать не менее 8 символов');
+                }
+                alert('Вы успешно авторизованы в системе');
+            } catch (error) {
+                alert(error.message);
+            }
+        })
     }
-})
+}
+const login = new Login();
+console.log(login.check());
--- SignUpPage/sign.js
+++ SignUpPage/sign.js
@@ -1,22 +1,28 @@
-const btn = document.querySelector('.button');
-const emailOrPhone = document.querySelector('.input1');
-const password = document.querySelector('.input2');
-const confirmPsw = document.querySelector('.input3');
+class SignUp {
+    registration() {
+        const btn = document.querySelector('.button');
 
-btn.addEventListener('click', function () {
-    try {
-        if (!/^[\w\.\_]+@[a-z]+\.+[a-z]{2,6}$||^[\+375\d]+$/g.test(emailOrPhone.value))
-            throw new Error('введите почту или номер телефона');
+        btn.addEventListener('click', function () {
+            const emailOrPhone = document.querySelector('.input1');
+            const password = document.querySelector('.input2');
+            const confirmPsw = document.querySelector('.input3');
+            try {
+                if (!/^[\w\.\-\$]+@[a-z]+\.[a-z]{2,4}|\+[0-9]{12}$/g.test(emailOrPhone.value))
+                    throw new Error('введите почту или номер телефона');
 
-        if (password.value.length < 8) {
-            throw new Error('пароль должен содержать не менее 8 символов');
-        }
+                if (password.value.length < 8) {
+                    throw new Error('пароль должен содержать не менее 8 символов');
+                }
 
-        if (password.value !== confirmPsw.value)
-            throw new Error('пароли должны совпадать');
+                if (password.value !== confirmPsw.value)
+                    throw new Error('пароли должны совпадать');
 
-        alert('Вы успешно зашли в систему');
-    } catch (error) {
-        alert(error.message);
+                alert('Вы успешно зашли в систему');
+            } catch (error) {
+                alert(error.message);
+            }
+        })
     }
-})
+}
+const signUp = new SignUp();
+console.log(signUp.registration())
